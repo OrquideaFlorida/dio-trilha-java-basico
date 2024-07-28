299ba3ed19da9a2f189337346e69e605747dfef0
Orquídea <orquideasilva09@yahoo.com.br>
2024-07-28 17:16:56 -0300

    Representação Iphone

---
+++ Projeto_Iphone/src/apps/NavegadorInternet.java
@@ -0,0 +1,9 @@
+package apps;
+
+public interface NavegadorInternet {
+    
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+    
+}
